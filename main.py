8dad0437378a72650851bc675870e575689197b6
kurozumi <info@a-zumi.net>
2021-03-12 17:51:55 +0900

    update

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class AliveMonitoring(unittest.TestCase):
 
     def test(self):
         r = requests.get(self.url, allow_redirects=False)
-        assert 200 == r.status_code, "{0} {1}".format(r.status_code, r.reason)
+        assert 500 == r.status_code, "{0} {1}".format(r.status_code, r.reason)
 
 if __name__ == "__main__":
     unittest.main()
